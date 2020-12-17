d939e6f5340e8fab2d40285c13802f560ece851f
Danny August Ramaputra <daystram@gmail.com>
2020-12-17 21:21:24 +1000

    Made config initialization explicity precedent, added handler initialization call

--- main.go
+++ main.go
@@ -5,18 +5,22 @@ import (
 	"net/http"
 	"time"
 
-	"github.com/daystram/go-gin-gorm-boilerplate/config"
-	"github.com/daystram/go-gin-gorm-boilerplate/router"
-
 	"github.com/gin-gonic/gin"
+
+	"github.com/daystram/go-gin-gorm-boilerplate/config"
+	"github.com/daystram/go-gin-gorm-boilerplate/handlers"
+	"github.com/daystram/go-gin-gorm-boilerplate/router"
 )
 
-func main() {
+func init() {
 	config.InitializeAppConfig()
 	if !config.AppConfig.Debug {
 		gin.SetMode(gin.ReleaseMode)
 	}
+}
 
+func main() {
+	handlers.InitializeHandler()
 	s := &http.Server{
 		Addr:           fmt.Sprintf(":%d", config.AppConfig.Port),
 		Handler:        router.InitializeRouter(),
@@ -24,5 +28,6 @@ func main() {
 		WriteTimeout:   10 * time.Second,
 		MaxHeaderBytes: 1 << 20,
 	}
+
 	s.ListenAndServe()
 }
