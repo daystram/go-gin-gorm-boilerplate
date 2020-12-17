6febc1898d0e2cb477109d788f63d381d0deeb9d
Danny August Ramaputra <daystram@gmail.com>
2020-12-17 21:20:51 +1000

    Created handler init, includes modules spin-up

--- handlers/init.go
+++ handlers/init.go
@@ -1,1 +1,52 @@
 package handlers
+
+import (
+	"fmt"
+	"log"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+
+	"github.com/daystram/go-gin-gorm-boilerplate/config"
+	"github.com/daystram/go-gin-gorm-boilerplate/models"
+)
+
+var Handler HandlerFunc
+
+type HandlerFunc interface {
+	RetrieveUser()
+}
+
+type module struct {
+	db *dbEntity
+}
+
+type dbEntity struct {
+	conn      *gorm.DB
+	userOrmer models.UserOrmer
+}
+
+func InitializeHandler() {
+	var err error
+
+	// Initialize DB
+	var db *gorm.DB
+	db, err = gorm.Open(postgres.Open(
+		fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
+			config.AppConfig.DBHostname, config.AppConfig.DBPort, config.AppConfig.DBDatabase,
+			config.AppConfig.DBUsername, config.AppConfig.DBPassword),
+	), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("[INIT] Failed connecting to PostgreSQL Database at %s:%d. %+v\n",
+			config.AppConfig.DBHostname, config.AppConfig.DBPort, err)
+	}
+	log.Printf("[INIT] Successfully connected to PostgreSQL Database\n")
+
+	// Compose handler modules
+	Handler = &module{
+		db: &dbEntity{
+			conn:      db,
+			userOrmer: models.NewUserOrmer(db),
+		},
+	}
+}
---
+++ handlers/user.go
@@ -0,0 +1,5 @@
+package handlers
+
+func (m *module) RetrieveUser() {
+
+}
