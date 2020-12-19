736f1ed2d52108f039338114d2924fe79208af5c
Danny August Ramaputra <daystram@gmail.com>
2020-12-19 16:40:54 +1000

    Implemented auth middleware

--- controllers/middleware/auth.go
+++ controllers/middleware/auth.go
@@ -1,8 +1,38 @@
 package middleware
 
-import "github.com/gin-gonic/gin"
+import (
+	"errors"
+	"net/http"
+	"strings"
+
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gin-gonic/gin"
+
+	"github.com/daystram/go-gin-gorm-boilerplate/config"
+	"github.com/daystram/go-gin-gorm-boilerplate/datatransfers"
+)
 
 func AuthMiddleware(c *gin.Context) {
-
+	token := strings.TrimPrefix(c.GetHeader("Authorization"), "Bearer ")
+	if token == "" {
+		c.AbortWithStatusJSON(http.StatusUnauthorized, datatransfers.Response{Error: "access token not provided"})
+		return
+	}
+	claims, err := parseToken(token, config.AppConfig.JWTSecret)
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusUnauthorized, datatransfers.Response{Error: err.Error()})
+		return
+	}
+	c.Set("authenticated", true)
+	c.Set("user_id", claims.ID)
 	c.Next()
+}
+
+func parseToken(tokenString, secret string) (claims datatransfers.JWTClaims, err error) {
+	if token, err := jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (interface{}, error) {
+		return []byte(secret), nil
+	}); err != nil || !token.Valid {
+		return datatransfers.JWTClaims{}, errors.New("failed parsing token")
+	}
+	return
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/daystram/go-gin-gorm-boilerplate
 go 1.15
 
 require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/fsnotify/fsnotify v1.4.9 // indirect
 	github.com/gin-contrib/cors v1.3.1
 	github.com/gin-gonic/gin v1.6.3
